c92da32a416d3d870a93849e6e84d2197e8bcba2
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-10-07 23:16:06 +0700

    update

--- src/app/Components/dropdownSite.tsx
+++ src/app/Components/dropdownSite.tsx
@@ -1,22 +1,24 @@
 import React, { useState } from 'react'
 
 const dropdownSite = () => {
-const [dropdownOpen, setDropdownOpen] = useState(false);
-const Sites = [
-    {title: 'SITE001 - Lahan Padi'},
-    {title: 'SITE002 - Lahan Padi'}
-]
-  
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const Sites = [
+    { title: 'SITE001 - Lahan Padi' },
+    { title: 'SITE002 - Lahan Padi' }
+  ];
+
   return (
     <div className="p-6">
       <span>Lokasi:</span>
-      <select name="site" className='bg-white font-bold '>
-        {Sites.map(option => (
-          <option className='bg-white' value={option.title}>{option.title} </option>
+      <select name="site" className="bg-white font-bold">
+        {Sites.map((option, index) => (
+          <option key={index} className="bg-white" value={option.title}>
+            {option.title}
+          </option>
         ))}
       </select>
     </div>
-  )
+  );
 }
 
-export default dropdownSite
+export default dropdownSite;
