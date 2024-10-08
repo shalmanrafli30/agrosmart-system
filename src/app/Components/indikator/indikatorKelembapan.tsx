2c95a519cddeeaf712c6af606d6222be6531478a
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-10-08 11:55:22 +0700

    edit range and add the svg icon

--- src/app/Components/indikator/indikatorKelembapan.tsx
+++ src/app/Components/indikator/indikatorKelembapan.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import Humidity from "../../assets/Humidity.svg";
 
 const indikatorKelembapan = ({ humid }: { humid: number }) => {
     const bgColor = 
-        humid > 30 || humid < 20 ? 'bg-warning' : 
+        humid > 60 || humid < 30 ? 'bg-warning' : 
         'bg-primary';
 
     const bgIcon =
-        humid > 30 || humid < 20 ? 'text-warningSecondary' : 
+        humid > 60 || humid < 30 ? 'text-warningSecondary' : 
         'text-secondary';
 
     return (
-        <div>indikatorKelembapan</div>
+        <div className={`${bgColor} w-full h-auto p-4 text-white rounded-xl relative overflow-hidden`}>
+            <div className="absolute inset-0 flex items-center justify-end z-0">
+                <Humidity className={`${bgIcon} w-1/4 mr-2`} />
+            </div>
+            <div className='relative z-10'>
+                <h5 className="mb-3">Suhu</h5>
+                <span className="font-bold text-4xl">{humid} °C</span>
+            </div>
+        </div>
     )
 }
 
