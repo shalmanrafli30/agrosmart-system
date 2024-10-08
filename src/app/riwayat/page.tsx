cff1077d4b3e39730c9f50b8d37c8ba64aba2edd
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-10-08 17:35:37 +0700

    add riwayat page

---
+++ src/app/riwayat/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Riwayat",
+    description: "AgroSmartSystem Realtime",
+};
+
+export default function page() {
+    return (
+        <div className="p-6">
+            <h3>RIWAYAT</h3>
+        </div>
+    )
+}
