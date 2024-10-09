5b6399ad8fa983029e73c5bbf36667b7569dfc5f
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-10-09 19:13:26 +0700

    modified satuan fosfor

--- src/app/Components/indikator/indikatorFosfor.tsx
+++ src/app/Components/indikator/indikatorFosfor.tsx
@@ -19,11 +19,11 @@ const indikatorFosfor: React.FC<indikatorFosforProps> = ({ fosfor, batasAtas, ba
     return (
         <div className={`${bgColor} w-full h-auto p-4 text-white rounded-xl relative overflow-hidden`}>
             <div className="absolute inset-0 flex items-center justify-end z-0">
-                <Fosfor className={`${bgIcon} w-1/5`} />
+                <Fosfor className={`${bgIcon} w-1/4`} />
             </div>
             <div className='relative z-10'>
                 <h5 className="mb-3">Fosfor</h5>
-                <span className="font-bold text-4xl">{fosfor} F</span>
+                <span className="font-bold text-4xl">{fosfor} P</span>
             </div>
         </div>
     )
