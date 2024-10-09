a66527332b50e96d07e053edc6387228270c4611
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-10-09 17:55:12 +0700

    change the vector size

--- src/app/Components/indikator/indikatorNitrogen.tsx
+++ src/app/Components/indikator/indikatorNitrogen.tsx
@@ -20,7 +20,7 @@ const indikatorNitrogen: React.FC<indikatorNitrogenProps> = ({nitrogen, batasAta
     return (
         <div className={`${bgColor} w-full h-auto p-4 text-white rounded-xl relative overflow-hidden`}>
             <div className="absolute inset-0 flex items-center justify-end z-0">
-                <Nitrogen className={`${bgIcon} w-1/3`} />
+                <Nitrogen className={`${bgIcon} w-1/4`} />
             </div>
             <div className='relative z-10'>
                 <h5 className="mb-3">Nitrogen</h5>
