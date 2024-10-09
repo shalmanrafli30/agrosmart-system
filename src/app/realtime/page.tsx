ae5172305caee698f8a3e0796e5bda4dd44de692
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-10-09 15:49:40 +0700

    modified

--- src/app/realtime/page.tsx
+++ src/app/realtime/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import SensorRealtime from "../Components/sensorRealtime";
 
 export const metadata: Metadata = {
     title: "Realtime",
@@ -8,7 +9,10 @@ export const metadata: Metadata = {
 export default function Realtime() {
     return (
         <div className="p-6">
-            <div className="bg-gray-600 flex-grow-[3] h-[500px] rounded-xl max-w-screen-2xl"></div>
+            <div className="bg-gray-600 flex-grow-[3] h-[500px] rounded-xl max-w-screen-2xl">
+
+            </div>
+            <SensorRealtime suhu={19} humid={30} />
         </div>
     )
 }
