f35e44bd2844e23cbd6ae32bb3dbaccc4c883c06
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-10-09 19:16:41 +0700

    change the batas-batas

--- src/app/Components/sensorRealtime.tsx
+++ src/app/Components/sensorRealtime.tsx
@@ -10,6 +10,7 @@ import IndikatorTds from './indikator/indikatorTDS';
 
 
 interface SensorRealtimeProps {
+    sensor: number;
     suhu: number;
     humid: number;
     nitrogen: number;
@@ -20,20 +21,20 @@ interface SensorRealtimeProps {
     tds: number;
 }
 
-const SensorRealtime: React.FC<SensorRealtimeProps> = ({ suhu, humid, nitrogen, fosfor, kalium, ph, ec, tds }) => {
+const SensorRealtime: React.FC<SensorRealtimeProps> = ({ sensor, suhu, humid, nitrogen, fosfor, kalium, ph, ec, tds }) => {
     return (
         <div className="mt-5">
-            <h3 className="font-bold text-2xl mb-5">Sensor 1</h3>
+            <h3 className="font-bold text-2xl mb-5">Sensor {sensor}</h3>
             <div className="bg-abu w-full rounded-md grid grid-cols-4 gap-2 p-4">
                 {/* THE CONTENT IS HERE */}
-                <IndikatorSuhu suhu={suhu} batasAtas={50} batasBawah={20}/>
-                <IndikatorKelembapan humid={humid} batasAtas={50} batasBawah={20}/>
+                <IndikatorSuhu suhu={suhu} batasAtas={30} batasBawah={20}/>
+                <IndikatorKelembapan humid={humid} batasAtas={60} batasBawah={30}/>
                 <IndikatorNitrogen nitrogen={nitrogen} batasAtas={40} batasBawah={20} />
-                <IndikatorFosfor fosfor={fosfor} batasAtas={50} batasBawah={20}/>
-                <IndikatorKalium kalium={kalium} batasAtas={50} batasBawah={20}/>
-                <IndikatorPh ph={ph} batasAtas={7} batasBawah={3}/>
-                <IndikatorEc ec={ec} batasAtas={50} batasBawah={20}/>
-                <IndikatorTds tds={tds} batasAtas={50} batasBawah={20}/>
+                <IndikatorFosfor fosfor={fosfor} batasAtas={30} batasBawah={15}/>
+                <IndikatorKalium kalium={kalium} batasAtas={200} batasBawah={100}/>
+                <IndikatorPh ph={ph} batasAtas={7} batasBawah={5}/>
+                <IndikatorEc ec={ec} batasAtas={3} batasBawah={1}/>
+                <IndikatorTds tds={tds} batasAtas={2000} batasBawah={500}/>
             </div>
         </div>
     );
