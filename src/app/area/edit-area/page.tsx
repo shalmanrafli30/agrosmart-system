4d94c5df84ea61fb2eebb99599b9dcd5065332c1
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-10-17 00:26:50 +0700

    add required to form input

--- src/app/area/edit-area/page.tsx
+++ src/app/area/edit-area/page.tsx
@@ -20,21 +20,21 @@ export default function page() {
                 <form action="#" className="max-w-screen-md mx-0 space-y-10">
                     <div id="idArea" className="flex space-x-5 items-center mb-2">
                         <label htmlFor="idArea" className="text-black text-base font-bold w-32">ID</label>
-                        <input type="text" name="idArea" id="" className="bg-white border border-abu3 text-black text-sm w-full p-2" value="SITE001"/>
+                        <input type="text" name="idArea" id="" className="bg-white border border-abu3 text-black text-sm w-full p-2" value="SITE001" required/>
                     </div>
                     <div id="namaArea" className="flex space-x-5 items-center mb-2">
                         <label htmlFor="namaArea" className="text-black text-base font-bold w-32">Nama Area</label>
-                        <input type="text" name="namaArea" id="" className="bg-white border border-abu3 text-black text-sm w-full p-2" value="Lingkungan"/>
+                        <input type="text" name="namaArea" id="" className="bg-white border border-abu3 text-black text-sm w-full p-2" value="Lingkungan" required/>
                     </div>
                     <div id="lokasi" className="flex space-x-5 items-center mb-2">
                         <label htmlFor="lokasi" className="text-black text-base font-bold w-32">Lokasi</label>
-                        <select name="lokasi" id="" className="bg-white border border-abu3 text-black text-sm w-full p-2 font-bold">
+                        <select name="lokasi" id="" className="bg-white border border-abu3 text-black text-sm w-full p-2 font-bold" required>
                             <option value="BPP SELAAWI">BPP SELAAWI</option>
                         </select>
                     </div>
                     <div id="tipe" className="flex space-x-5 items-center mb-2">
                         <label htmlFor="tipe" className="text-black text-base font-bold w-32">Tipe</label>
-                        <select name="tipe" id="" className="bg-white border border-abu3 text-black text-sm w-full p-2 font-bold">
+                        <select name="tipe" id="" className="bg-white border border-abu3 text-black text-sm w-full p-2 font-bold" required>
                             <option value="env">env</option>
                             <option value="soil">soil</option>
                         </select>
