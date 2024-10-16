77564d6297be47057c8c71cfc3c3ab67d5c60963
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-10-17 01:39:45 +0700

    add chart to riwayat page

--- src/app/riwayat/page.tsx
+++ src/app/riwayat/page.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Chart from '../Components/Chart';
+import Site from "../Components/dropdownSite";
 
 export const metadata: Metadata = {
     title: "Riwayat",
@@ -8,7 +10,41 @@ export const metadata: Metadata = {
 export default function page() {
     return (
         <div className="p-6">
-            <h3>RIWAYAT</h3>
+            <div className="flex justify-between items-center w-full mb-4">
+                <Site />
+                <span className="text-right">Update Terakhir: 16/10/2024 21:35 PM</span>
+            </div>
+            <div className="mb-6 text-left">
+                <form className="max-w-fit">
+                    <div className="flex mb-4 w-fit">
+                        <span className="inline-flex items-center px-3 text-sm text-black font-semibold bg-primary border border-e-0 border-primary rounded-s-md">
+                            Area:
+                        </span>
+                        <select name="area" id="area" className="rounded-none rounded-e-lg bg-white border border-primary text-black block flex-1 min-w-0 w-full text-sm p-2.5 focus:ring-transparent">
+                            <option value="Lingkungan">Lingkungan</option>
+                            <option value="Padi">Sensor 1</option>
+                            <option value="Padi">Sensor 2</option>
+                        </select>
+                    </div>
+                    <div className="flex items-center space-x-5 mb-4">
+                        <div className="flex">
+                            <span className="inline-flex items-center px-3 text-sm text-black font-semibold bg-primary border border-e-0 border-primary rounded-s-md">
+                                Dari:
+                            </span>
+                            <input type="datetime-local" name="dari" id="" className="rounded-none rounded-e-lg bg-white border border-primary text-black block flex-1 min-w-0 w-full text-sm p-2.5 focus:ring-transparent"/>
+                        </div>
+                        <span className="font-bold text-2xl">-</span>
+                        <div className="flex">
+                            <span className="inline-flex items-center px-3 text-sm text-black font-semibold bg-primary border border-e-0 border-primary rounded-s-md">
+                                Ke:
+                            </span>
+                            <input type="datetime-local" name="dari" id="" className="rounded-none rounded-e-lg bg-white border border-primary text-black block flex-1 min-w-0 w-full text-sm p-2.5 focus:ring-transparent"/>
+                        </div>
+                    </div>
+                    <input type="submit" value="Kirim" className="bg-primary text-black font-semibold text-sm rounded-md p-3 cursor-pointer hover:bg-secondary w-2/12"/>
+                </form>
+            </div>
+            <Chart />
         </div>
     )
 }
