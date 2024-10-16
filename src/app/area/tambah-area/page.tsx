b857d9406feb5267e8736dba326601289577ffdd
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-10-16 22:49:39 +0700

    add tambah-area page

---
+++ src/app/area/tambah-area/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Tambah Area",
+    description: "AgroSmartSystem Area",
+};
+
+export default function page() {
+    return(
+        <div className="p-6">
+            <h3>TES</h3>
+        </div>
+    )
+}
