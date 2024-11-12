91d0af1d0f375d8ebc23e65f66f7dc105bd8728d
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-11-12 17:39:13 +0700

    remove header title on layout for sensor

--- src/app/sensor/layout.tsx
+++ src/app/sensor/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <RootLayout>
-    <Header title={'Area'}/>
+    {/* <Header title={'Area'}/> */}
       {children}
     </RootLayout>; // Use the client component here
 }
